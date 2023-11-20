363c6ed6a060994d36151c1f91ce2404e638f5a4
AphroditeF <a.flora@outlook.com>
2023-11-20 18:14:24 +0200

    Create Ticket Class and Implement Equals Method from IEquatable

--- TicketingSolution/Program.cs
+++ TicketingSolution/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+            Console.WriteLine(t2.Equals(t1));
         }
     }
 }
